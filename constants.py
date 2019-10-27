5bb829d609ef25109869258fd04f97b35b4a1a0f
Jean-Marc Orliaguet <jmo@chalmers.se>
2019-10-27 09:24:29 +0100

    Updated common words

--- constants.py
+++ constants.py
@@ -1,7 +1,10 @@
 
 # Custom categories
 SKIP_PROJECTS = 'Chalmers IT-avdelning', 'Servicedesk'
-COMMON_WORDS = 'tack', 'thanks', 'hi', 'hello', 'hej'
+COMMON_WORDS = ('tack', 'thanks', 'hi', 'hello', 'hej', 'chalmers',
+                'http', 'www', 'ska', 'göra', 'php', 'hälsningar', 'tekniska',
+                'högskola', 'auml', 'padding', 'helvetica', 'table', 'font',
+                'hogskola')
 
 # Data location on filesystem
 DATA_PATH = "./data"
